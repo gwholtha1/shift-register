1c3ea505cc846a8789759dd67c5555fbbf11dc79
gwholtha1 <47867003+gwholtha1@users.noreply.github.com>
2019-08-12 11:49:11 -0700

    Create shiftReg8.v

---
+++ shiftReg8.v
@@ -0,0 +1,21 @@
+module shiftReg4(
+  input shift_in,
+  input clock,
+  output shift_out
+)
+
+reg bit0;
+reg bit1;
+reg bit2;
+reg bit3;
+
+assign shift_out = bit3;
+
+always @(posedge clock) begin
+  bit3 <= bit2;
+  bit2 <= bit1;
+  bit1 <= bit0;
+  bit0 <= shift_in;
+end
+
+endmodule
